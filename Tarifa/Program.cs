4d9ec7f7e0762e4761849d85164145145b8406e6
andreas domhagen <andreas.domhagen@live.se>
2021-03-11 12:39:17 +0100

    Started on Tarifa

---
+++ Tarifa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theseMonths = Convert.ToInt32(Console.ReadLine());
+            int megaBytes = 100;
+            int megaBytesPerMonth()
+            {
+                int total = 0;
+                for (int i = 0; i < theseMonths; i++)
+                {
+                    Console.WriteLine($"Mb used month {i + 1}");
+                    int MbEveryMonth = Convert.ToInt32(Console.ReadLine());
+                    total = total + MbEveryMonth;
+                }
+                return total;
+            }
+            Console.WriteLine($"{megaBytesPerMonth()}");
+        }
+    }
+}
