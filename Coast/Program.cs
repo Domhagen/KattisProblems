38acab7fa995e72b6f7f3e3f0a2ba1c9f5162c69
Andreas Domhagen <andreas.domhagen@live.se>
2021-04-18 12:28:28 +0200

    added another example in Coast

---
+++ Coast/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace Coast
+{
+    class Program
+    {
+        static int a;
+        static int e;
+
+        static void Main(string[] args)
+        {
+            int[][] coastGrid = new int[5][];
+
+
+
+            coastGrid[0] = new int[] { 0, 1, 1, 1, 1, 0 };
+            coastGrid[1] = new int[] { 0, 1, 0, 1, 1, 0 };
+            coastGrid[2] = new int[] { 1, 1, 1, 0, 0, 0 };
+            coastGrid[3] = new int[] { 0, 0, 0, 0, 1, 0 };
+            coastGrid[4] = new int[] { 0, 0, 0, 0, 0, 0 };
+
+
+            int coastLines = 0;
+            //coastGrid[a][e] = 1 ;
+
+            calcCoastLines(coastGrid);
+            //checkForLakes(coastGrid);
+            Console.WriteLine($"number of coast lines are {coastLines}");
+
+            int calcCoastLines(int[][] jaggedArray)
+            {
+                foreach (int[] array in jaggedArray)
+                {
+                    foreach (var value in array)
+                    {
+                        if (value == 1)
+                        {
+                            try
+                            {
+                                if (jaggedArray[+1][array[value]] == 1)
+                                {
+                                    coastLines = coastLines + 0;
+                                }
+                                else if (jaggedArray[+1][array[value]] == 0)
+                                {
+                                    coastLines++;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                coastLines++;
+                            }
+                            try
+                            {
+                                if (coastGrid[-1][value] == 1)
+                                {
+                                    coastLines = coastLines + 0;
+                                }
+                                else if (coastGrid[-1][value] == 0)
+                                {
+                                    coastLines++;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                coastLines++;
+                            }
+                            try
+                            {
+                                if (array[value+1] == 1)
+                                {
+                                    coastLines = coastLines + 0;
+                                }
+                                else if (array[value+1] == 0)
+                                {
+                                    coastLines++;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                coastLines++;
+                            }
+                            try
+                            {
+                                if (array[value - 1] == 0)
+                                {
+                                    coastLines++;
+                                }
+                                else if (array[value - 1] == 1)
+                                {
+                                    coastLines = coastLines + 0;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                coastLines++;
+                            }
+                        }
+                    }
+                }
+                return coastLines;
+            }
+            //int checkForLakes(int[][] jaggedArray)
+            //{
+            //    foreach (int[] array in jaggedArray)
+            //    {
+            //        foreach (var value in array)
+            //        {
+            //            if (value == 0)
+            //            {
+            //                int lakeLines = 0;
+            //                try
+            //                {
+            //                    if (jaggedArray[a + 1][e] == 1)
+            //                    {
+            //                        lakeLines++;
+            //                    }
+            //                    else if (jaggedArray[a + 1][e] == 0)
+            //                    {
+            //                        lakeLines = lakeLines + 0;
+            //                    }
+            //                }
+            //                catch (Exception)
+            //                {
+            //                    lakeLines++;
+            //                }
+            //                try
+            //                {
+            //                    if (jaggedArray[a - 1][e] == 1)
+            //                    {
+            //                        lakeLines++;
+            //                    }
+            //                    else if (jaggedArray[a - 1][e] == 0)
+            //                    {
+            //                        lakeLines = lakeLines + 0;
+            //                    }
+            //                }
+            //                catch (Exception)
+            //                {
+            //                    lakeLines++;
+            //                }
+            //                try
+            //                {
+            //                    if (jaggedArray[a][e + 1] == 1)
+            //                    {
+            //                        lakeLines++;
+            //                    }
+            //                    else if (jaggedArray[a][e + 1] == 0)
+            //                    {
+            //                        lakeLines = lakeLines + 0;
+            //                    }
+            //                }
+            //                catch (Exception)
+            //                {
+            //                    lakeLines++;
+            //                }
+            //                try
+            //                {
+            //                    if (jaggedArray[a][e - 1] == 1)
+            //                    {
+            //                        lakeLines++;
+            //                    }
+            //                    else if (jaggedArray[a][e - 1] == 0)
+            //                    {
+            //                        lakeLines = lakeLines + 0;
+            //                    }
+            //                }
+            //                catch (Exception)
+            //                {
+            //                    lakeLines++;
+            //                }
+            //                if(lakeLines >= 4)
+            //                {
+            //                    return coastLines = coastLines - 4;
+            //                }
+            //            }
+            //        }
+            //    }
+            //    return coastLines;
+            //}
+        }
+    }
+}
---
+++ SpeedLimit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedLimit
+{
+    class Program
+    {
+        static int hoursDriven;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total time driving");
+            int totalHoursDriven = int.Parse(Console.ReadLine());
+
+            List<int> casesTimeDriven = new List<int>();
+
+            List<int> speedCases = new List<int>();
+
+            int a = 1;
+            while(totalHoursDriven > hoursDriven)
+            {
+                Console.WriteLine($"Speed in case {a}");
+                int speedCase = int.Parse(Console.ReadLine());
+                speedCases.Add(speedCase);
+                Console.WriteLine($"Hours in case {a}");
+                int hoursCase = int.Parse(Console.ReadLine());
+                speedCases.Add(hoursCase);
+
+                int caseTimeDriven = speedCase * hoursCase;
+                casesTimeDriven.Add(caseTimeDriven);
+
+                a++;
+
+                hoursDriven = hoursDriven + hoursCase;
+            }
+
+            int total = 0;
+            foreach(int item in casesTimeDriven)
+            {
+                total = total + item;
+            }
+            
+            Console.WriteLine(total);
+        }
+    }
+}
